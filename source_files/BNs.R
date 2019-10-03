865cbb75b2e179bd5646fac2e52f224f7d7e09cd
Issoufou-Liman <issoufoul@gmail.com>
2019-10-03 06:20:40 +0300

    adding bns and modis option files

--- source_files/BNs.R
+++ source_files/BNs.R
@@ -1,12 +1,14 @@
+## The bayesian Net to be used ####
+
 ## flow accummulation node: this will be a raster ####
-f_acc_node <- cptable(~f_acc, values = c(0.2, 0.2, 0.2, 0.2, 0.2), levels = c('Very low', "Low","Medium", "High", "Very high"))
+f_acc_node <- cptable(~f_acc, values = c(0.2, 0.2, 0.2), levels = c("Low","Medium", "High"))
 
 ## slope node: this will be a raster ####
-slope_node <- cptable(~slope, values = c(0.2, 0.2, 0.2, 0.2, 0.2), levels = c("Very steep", "Step", "Gentile", "Relatively flat", "Flat"))
+slope_node <- cptable(~slope, values = c(0.2, 0.2, 0.2, 0.2, 0.2), levels = c("V. steep", "Step", "Gentile", "R. flat", "Flat"))
 
 ## suitable topography: this will be a just probabilities (Not a raster) ####
-suitable_topo_node_tmp <- make_gRain_CPT(parent_effects = list(c(0, 0, 2, 3, 4),
-                                                               c(0, 0, 1, 3, 5)),
+suitable_topo_node_tmp <- make_gRain_CPT(parent_effects = list(c(2, 3, 1),
+                                                               c(0, 0, 1, 2, 3)),
                                          parent_weights = c(3, 2),
                                          b = 2,
                                          child_prior = c(0.80, 0.20),
@@ -19,18 +21,18 @@ suitable_topo_node_levels <- suitable_topo_node_tmp$levels
 suitable_topo_node <- cptable (~suitable_topo|f_acc:slope, values = suitable_topo_node_values,levels = suitable_topo_node_levels)
 
 ## soil water node : this will be a raster ####
-soil_water_content_node <- cptable(~soil_water_content, values = c(0.2, 0.2, 0.2, 0.2, 0.2), levels = c("Very low", "Low", "Medium", 'High', 'Very high'))
+soil_water_content_node <- cptable(~soil_water_content, values = c(0.2, 0.2, 0.2, 0.2, 0.2), levels = c("V. low", "Low", "Medium", 'High', 'V. high'))
 
 ## wetness exposure: this will be a raster  ####
-exposure2wetness_node <- cptable(~exposure2wetness, values = c(0.2, 0.2, 0.2, 0.2, 0.2), levels = c("Very low", "Low", "Medium", 'High', 'Very high'))
+exposure2wetness_node <- cptable(~exposure2wetness, values = c(0.2, 0.2, 0.2, 0.2, 0.2), levels = c("V. low", "Low", "Medium", 'High', 'V. high'))
 
 ## suitable soils this will be a raster ####
-suitable_soil_node_tmp <- make_gRain_CPT(parent_effects = list(c(0, 0.5, 2, 3.5, 4),
-                                                               c(0, 0.5, 2, 3.5, 4)),
+suitable_soil_node_tmp <- make_gRain_CPT(parent_effects = list(c(0, 1, 2, 3, 0),
+                                                               c(0, 1, 2, 3, 0)),
                                          parent_weights = c(3, 1),
                                          b = 5,
                                          child_prior = c(0.1, 0.3, 0.4, 0.3, 0.1),
-                                         child_states = c("Unusable", "Unsuitable", "Moderately suitable", "suitable", "Highly suitable"),
+                                         child_states = c("Unusable", "Unsuitable", "M. suitable", "suitable", "H. suitable"),
                                          parent_states = list(soil_water_content_node$levels,
                                                               exposure2wetness_node$levels),
                                          parent_names = c('soil_water_content', 'exposure2wetness'))
@@ -39,18 +41,18 @@ suitable_soil_node_levels <- suitable_soil_node_tmp$levels
 suitable_soil_node <- cptable (~suitable_soil|soil_water_content:exposure2wetness, values = suitable_soil_node_values,levels = suitable_soil_node_levels)
 
 ## sensitivity to flood ####
-sensitivity2flood_node <- cptable(~sensitivity2flood, values = c(0.2, 0.2, 0.2, 0.2, 0.2), levels = c("Very low", "Low", "Medium", 'High', 'Very high'))
+sensitivity2flood_node <- cptable(~sensitivity2flood, values = c(0.2, 0.2, 0.2, 0.2, 0.2), levels = c("V. low", "Low", "Medium", 'High', 'V. high'))
 
 ## vegetation sensitibity to water ####
-veg_sensitivity2water_var_node <- cptable(~veg_sensitivity2water_var, values = c(0.2, 0.2, 0.2, 0.2, 0.2), levels = c("Very low", "Low", "Medium", 'High', 'Very high'))
+veg_sensitivity2water_var_node <- cptable(~veg_sensitivity2water_var, values = c(0.2, 0.2, 0.2, 0.2, 0.2), levels = c("V. low", "Low", "Medium", 'High', 'V. high'))
 
 ## characteristic vegetation ####
-charact_veg_node_tmp <- make_gRain_CPT(parent_effects = list(c(0, 0.5, 2, 3.5, 4),
-                                                             c(0, 0.5, 2, 3.5, 4)),
-                                       parent_weights = c(1, 3),
+charact_veg_node_tmp <- make_gRain_CPT(parent_effects = list(c(1, 1, 2, 4, 5),
+                                                             c(1, 1, 2, 4, 5)),
+                                       parent_weights = c(1, 2),
                                        b = 5,
                                        child_prior = c(0.1, 0.3, 0.4, 0.3, 0.1),
-                                       child_states = c('Dry', "Dehydrate", "Moderately hydrated", "hydrated", 'Water logged'),
+                                       child_states = c('Dry', "Dehydrate", "M. hydrated", "hydrated", 'Water logged'),
                                        parent_states = list(sensitivity2flood_node$levels,
                                                             veg_sensitivity2water_var_node$levels),
                                        parent_names = c('sensitivity2flood', 'veg_sensitivity2water_var'))
@@ -58,59 +60,51 @@ charact_veg_node_values <- charact_veg_node_tmp$values
 charact_veg_node_levels <- charact_veg_node_tmp$levels
 charact_veg_node <- cptable (~charact_veg |sensitivity2flood:veg_sensitivity2water_var, values = charact_veg_node_values,levels = charact_veg_node_levels)
 
-## power of tools: this will be a raster ####
-p_tools_node <- cptable(~p_tools, values = c(0.20, 0.80), levels = c("Low", 'High'))
-
 ## water present: this will be a raster ####
-water_prznt_node_tmp <- make_gRain_CPT(parent_effects = list(c(0, 1),
-                                                             c(0, 1, 2, 4, 5),
-                                                             c(0, 1, 2, 4, 5),
-                                                             c(0, 1, 2, 4, 5)),
-                                       parent_weights = c(3, 1, 2, 3),
+water_prznt_node_tmp <- make_gRain_CPT(parent_effects = list(c(1, 1, 2, 4, 5),
+                                                             c(1, 2, 3, 4, 1),
+                                                             c(1, 2, 3, 4, 1)),
+                                       parent_weights = c(3, 2, 2),
                                        b = 3,
                                        child_prior = c(0.15, 0.35, 0.35, 0.15),
                                        child_states = c('Unexpected', 'Unsual', 'Commun', 'Permanent'),
-                                       parent_states = list(p_tools_node$levels,
-                                                            sensitivity2flood_node$levels,
+                                       parent_states = list(sensitivity2flood_node$levels,
                                                             exposure2wetness_node$levels,
                                                             soil_water_content_node$levels),
-                                       parent_names = c('p_tools', 'sensitivity2flood', 'exposure2wetness', 'soil_water_content'))
+                                       parent_names = c('sensitivity2flood', 'exposure2wetness', 'soil_water_content'))
 water_prznt_node_values <- water_prznt_node_tmp$values
 water_prznt_node_levels <- water_prznt_node_tmp$levels
-water_prznt_node <- cptable (~water_prznt|p_tools:sensitivity2flood:exposure2wetness:soil_water_content, values = water_prznt_node_values,levels = water_prznt_node_levels)
-
+water_prznt_node <- cptable (~water_prznt|sensitivity2flood:exposure2wetness:soil_water_content, values = water_prznt_node_values,levels = water_prznt_node_levels)
 
 ## temporal variation in vegetation indices: this will be a raster ####
 
 ## water present: this will be a raster ####
-tempo_var_vi_node_tmp <- make_gRain_CPT(parent_effects = list(c(1, 0),
-                                                              c(0, 1, 2, 4, 5),
-                                                              c(0, 1, 2, 4, 5),
-                                                              c(0, 1, 2, 4, 5)),
-                                        parent_weights = c(1, 3, 2, 3),
+tempo_var_vi_node_tmp <- make_gRain_CPT(parent_effects = list(c(0, 1, 2, 4, 5),
+                                                              c(0, 1, 2, 4, 0),
+                                                              c(0, 1, 2, 4, 0)),
+                                        parent_weights = c(3, 2, 3),
                                         b = 3,
                                         child_prior = c(0.05, 0.1, 0.35, 0.35, 0.05, 0.1),
-                                        child_states = c('Extremely low', "Very low", "Low", 'High', 'Very high', 'Extremely high'),
-                                        parent_states = list(p_tools_node$levels,
-                                                             sensitivity2flood_node$levels,
+                                        child_states = c('Ext. low', "V. low", "Low", 'High', 'V. high', 'E. high'),
+                                        parent_states = list(sensitivity2flood_node$levels,
                                                              exposure2wetness_node$levels,
                                                              soil_water_content_node$levels),
-                                        parent_names = c('p_tools', 'sensitivity2flood', 'exposure2wetness', 'soil_water_content'))
+                                        parent_names = c('sensitivity2flood', 'exposure2wetness', 'soil_water_content'))
 tempo_var_vi_node_values <- tempo_var_vi_node_tmp$values
 tempo_var_vi_node_levels <- tempo_var_vi_node_tmp$levels
-tempo_var_vi_node <- cptable (~tempo_var_vi|p_tools:sensitivity2flood:exposure2wetness:soil_water_content, values = tempo_var_vi_node_values,levels = tempo_var_vi_node_levels)
+tempo_var_vi_node <- cptable (~tempo_var_vi|sensitivity2flood:exposure2wetness:soil_water_content, values = tempo_var_vi_node_values,levels = tempo_var_vi_node_levels)
 
 ## flood at the beginning of the vegetation period: this will be a raster ####
-flood_bgs_node <- cptable(~flood_bgs, values = c(0.2, 0.2, 0.2, 0.2, 0.2), levels = c('Unexpected', 'Unsual', 'Commun', 'Frequent', 'Permanent'))
+flood_bgs_node <- cptable(~flood_bgs, values = c(0.2, 0.2, 0.2, 0.2), levels = c('Unexpected', 'Commun', 'Frequent', 'Permanent'))
 
 ## flooded at some points: this will be a just probabilities (Not a raster) ####
 flooded_at_some_pts_node_tmp <- make_gRain_CPT(parent_effects = list(c(0, 0, 1, 3, 4),
-                                                                     c(0, 0.5, 2, 3),
+                                                                     c(0, 1, 2, 3),
                                                                      c(0, 0, 1, 3, 4)),
                                                parent_weights = c(1, 2, 1),
                                                b = 3,
                                                child_prior = c(0.1, 0.3, 0.4, 0.3, 0.1),
-                                               child_states = c("Very low", "Low", "Medium", 'High', 'Very high'),
+                                               child_states = c("V. low", "Low", "Medium", 'High', 'V. high'),
                                                parent_states = list(suitable_soil_node$levels,
                                                                     water_prznt_node$levels,
                                                                     charact_veg_node$levels),
@@ -120,14 +114,14 @@ flooded_at_some_pts_node_levels <- flooded_at_some_pts_node_tmp$levels
 flooded_at_some_pts_node <- cptable (~flooded_at_some_pts|suitable_soil:water_prznt:charact_veg, values = flooded_at_some_pts_node_values,levels = flooded_at_some_pts_node_levels)
 
 ## Vegetation variation caused by flood: this will be a just probabilities (Not a raster) ####
-veg_var_caused_by_flood_node_tmp <- make_gRain_CPT(parent_effects = list(c(0, 0, 0.5, 1, 3, 4),
-                                                                         c(0, 0, 2, 3, 4),
-                                                                         c(0, 0, 2, 3, 4),
-                                                                         c(0, 0, 1, 2)),
+veg_var_caused_by_flood_node_tmp <- make_gRain_CPT(parent_effects = list(c(1, 2, 3, 4, 5, 6),
+                                                                         c(0, 1, 2, 3, 0),
+                                                                         c(2, 3, 1),
+                                                                         c(0, 1, 2, 0)),
                                                    parent_weights = c(2, 1, 2, 1),
                                                    b = 3,
                                                    child_prior = c(0.1, 0.3, 0.4, 0.3, 0.1),
-                                                   child_states = c('Very low', 'Low', 'Medium', "High", 'Very high'),
+                                                   child_states = c('V. low', 'Low', 'Medium', "High", 'V. high'),
                                                    parent_states = list(tempo_var_vi_node$levels, 
                                                                         flood_bgs_node$levels,
                                                                         f_acc_node$levels,
@@ -138,35 +132,24 @@ veg_var_caused_by_flood_node_levels <- veg_var_caused_by_flood_node_tmp$levels
 veg_var_caused_by_flood_node <- cptable (~veg_var_caused_by_flood|tempo_var_vi:flood_bgs:f_acc:water_prznt, values = veg_var_caused_by_flood_node_values,levels = veg_var_caused_by_flood_node_levels)
 
 ## FBFS/FBS: this is the target node and this will be a just probabilities (Not a raster)####
-# pixel_is_FBFS_node_tmp <- make_gRain_CPT(parent_effects = list(c(0, 1),
-#                                                                c(0, 0, 1, 3, 4),
-#                                                                c(0, 0, 1, 3, 4)),
-#                                          parent_weights = c(3, 1, 2),
-#                                          b = 4,
-#                                          child_prior = c(0.1, 0.3, 0.4, 0.3, 0.1),
-#                                          child_states = c('Highly unlikely', 'Unlikely', 'Possible', "Likely", "Highly likely"),
-#                                          parent_states = list(suitable_topo_node$levels,
-#                                                               flooded_at_some_pts_node$levels,
-#                                                               veg_var_caused_by_flood_node$levels),
-#                                          parent_names = c('suitable_topo', 'flooded_at_some_pts', 'veg_var_caused_by_flood'))
-# pixel_is_FBFS_node_values <- pixel_is_FBFS_node_tmp$values
-# pixel_is_FBFS_node_levels <- pixel_is_FBFS_node_tmp$levels
-# pixel_is_FBFS_node <- cptable (~pixel_is_FBFS|suitable_topo:flooded_at_some_pts:veg_var_caused_by_flood, values = pixel_is_FBFS_node_values,levels = pixel_is_FBFS_node_levels)
+p_tools_node <- cptable(~p_tools, values = c(0.20, 0.80), levels = c("Low", 'High'))
 
 pixel_is_FBFS_node_tmp <- make_gRain_CPT(parent_effects = list(c(0, 1),
                                                                c(0, 0, 1, 3, 4),
-                                                               c(0, 0, 1, 3, 4)),
-                                         parent_weights = c(3, 1, 2),
+                                                               c(0, 0, 1, 3, 4),
+                                                               c(0, 1)),
+                                         parent_weights = c(3, 1, 2, 3),
                                          b = 4,
                                          child_prior = c(0.05, 0.4, 0.6, 0.4, 0.05),
-                                         child_states = c('Highly unlikely', 'Unlikely', 'Possible', "Likely", "Highly likely"),
+                                         child_states = c('H. unlikely', 'Unlikely', 'Possible', "Likely", "H. likely"),
                                          parent_states = list(suitable_topo_node$levels,
                                                               flooded_at_some_pts_node$levels,
-                                                              veg_var_caused_by_flood_node$levels),
-                                         parent_names = c('suitable_topo', 'flooded_at_some_pts', 'veg_var_caused_by_flood'))
+                                                              veg_var_caused_by_flood_node$levels,
+                                                              p_tools_node$levels),
+                                         parent_names = c('suitable_topo', 'flooded_at_some_pts', 'veg_var_caused_by_flood', 'p_tools'))
 pixel_is_FBFS_node_values <- pixel_is_FBFS_node_tmp$values
 pixel_is_FBFS_node_levels <- pixel_is_FBFS_node_tmp$levels
-pixel_is_FBFS_node <- cptable (~pixel_is_FBFS|suitable_topo:flooded_at_some_pts:veg_var_caused_by_flood, values = pixel_is_FBFS_node_values,levels = pixel_is_FBFS_node_levels)
+pixel_is_FBFS_node <- cptable (~pixel_is_FBFS|suitable_topo:flooded_at_some_pts:veg_var_caused_by_flood:p_tools, values = pixel_is_FBFS_node_values,levels = pixel_is_FBFS_node_levels)
 
 ## compiling the Bayesian network ####
 
